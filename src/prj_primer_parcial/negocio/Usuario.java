7e03f9348755bdb0e77e962312976f28eb063c03
Mateo Ortiz <bptecgaming@outlook.com>
2024-06-08 19:15:49 -0500

    Eliminar usuarios

--- src/prj_primer_parcial/negocio/Usuario.java
+++ src/prj_primer_parcial/negocio/Usuario.java
@@ -1,20 +1,30 @@
-// Usuario.java
 package prj_primer_parcial.negocio;
 
 import java.io.Serializable;
 
 public class Usuario implements Serializable {
     private static final long serialVersionUID = 1L;
+    private String Id;
     private String usuario;
     private String password;
 
     // Constructor
-    public Usuario(String usuario, String password) {
+    public Usuario(String Id, String usuario, String password) {
+        this.Id = Id;
         this.usuario = usuario;
         this.password = password;
     }
 
     // Getters y Setters
+    
+    public String getId() {
+        return Id;
+    }
+    
+    public void setId(String Id) {
+        this.Id = Id;
+    }
+    
     public String getUsuario() {
         return usuario;
     }
--- src/prj_primer_parcial/persistencia/UsuarioDAO.java
+++ src/prj_primer_parcial/persistencia/UsuarioDAO.java
@@ -26,19 +26,19 @@ public class UsuarioDAO {
              PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, usuario.getUsuario());
             ps.setString(2, usuario.getPassword());
-            ps.setString(3, usuario.getUsuario()); // Corrige esta línea
+            ps.setString(3, usuario.getId());
             ps.executeUpdate();
         }
     }
 
     public void eliminarUsuario(String id_use) throws SQLException {
-        String sql = "DELETE FROM user WHERE id_use = ?";
-        try (Connection con = DBConnection.getConnection();
-             PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setString(1, id_use);
-            ps.executeUpdate();
-        }
+    String sql = "DELETE FROM user WHERE id_use = ?";
+    try (Connection con = DBConnection.getConnection();
+         PreparedStatement ps = con.prepareStatement(sql)) {
+        ps.setString(1, id_use);
+        ps.executeUpdate();
     }
+}
 
     public List<Usuario> obtenerUsuarios() throws SQLException {
         List<Usuario> usuarios = new ArrayList<>();
@@ -48,6 +48,7 @@ public class UsuarioDAO {
              ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
                 Usuario usuario = new Usuario(
+                        rs.getString("id_use"),
                         rs.getString("usuario"),
                         rs.getString("password")
                 );
@@ -66,6 +67,7 @@ public class UsuarioDAO {
             try (ResultSet rs = ps.executeQuery()) {
                 while (rs.next()) {
                     Usuario user = new Usuario(
+                            rs.getString("id_use"),
                             rs.getString("usuario"),
                             rs.getString("password")
                     );
